fbe30fe53173835df14d1ea5538e9b0f990e0c8d
Joonil Kim <bartholomaeuskim@gmail.com>
2022-12-07 12:05:18 -0700

    search order modified

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .config import Config
 from .models import db, Symbol
+from sqlalchemy import case
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,6 +9,6 @@ db.init_app(app)
 
 @app.route('/api/search/<keyword>')
 def index(keyword):
-    result = [{'symbol': item.symbol, 'name': item.name} for item in Symbol.query.filter(Symbol.symbol.like(f'%{keyword}%') | Symbol.name.like(f'%{keyword}%')).limit(7)]
+    result = [{'symbol': item.symbol, 'name': item.name} for item in Symbol.query.filter(Symbol.symbol.like(f'%{keyword}%') | Symbol.name.like(f'%{keyword}%')).order_by(case((Symbol.symbol.startswith(keyword), 0), (Symbol.name.startswith(keyword), 1), else_=2)).limit(7)]
 
     return result
